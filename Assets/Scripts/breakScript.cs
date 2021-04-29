13693ff071cc3415517f15fb92f6d40119469ef5
CommanderKeef <danny.mazzotti@gmail.com>
2021-04-29 16:25:03 +0800

    Prop Fixes and starting related Scripts
    Fucking annoyinggggggggggggggggggg

---
+++ Assets/Scripts/breakScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakScript : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision col)
+    {
+        GameObject body = col.gameObject;
+        Rigidbody rb = body.GetComponent<Rigidbody>();
+        GameObject parent = body.transform.parent.gameObject;
+        float vel = rb.velocity.magnitude;
+        Debug.Log(parent.name);
+        if(parent.name == "Dishes" && vel > 0.6f)
+        {
+            GameObject ass = Instantiate(Resources.Load("GlassBroke"), Quaternion.identity as GameObject);
+            body.Destroy(body);
+        }
+    }
+}
