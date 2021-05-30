8193b834f38e98981297c942415a0e751e440579
Curtis Bradford <curtis@goldkidz.com>
2021-05-30 12:12:01 +0800

    Stashed changes #2
    Some updates to the knockables script

--- Assets/Scripts/Knockable.cs
+++ Assets/Scripts/Knockable.cs
@@ -9,7 +9,7 @@ public class Knockable : MonoBehaviour
     public float hitForce = 1f;
     public float maxDistance = 1f;
     public float lastStep = 0.3f;         //used for controlling the firerate of the guns
-    float timeBetweenSteps = 0.3f;
+    //float timeBetweenSteps = 0.3f;
     int forceDirection;
     Vector3 swipeDirection;
     private GameObject heldItem;
@@ -20,40 +20,43 @@ public class Knockable : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void Update()
+    void Update()
     {
-            
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //forceDirection = Random.Range(1, 3);
-
-            if (Physics.Raycast(ray, out hit, maxDistance))
+        if(Time.timeScale != 0f)
+        { 
+            if (Input.GetKeyDown(KeyCode.Mouse1))
             {
-                if (hit.rigidbody != null)
-                {                     
-                    hit.rigidbody.velocity = Vector3.zero;
-                    hit.rigidbody.AddForceAtPosition(transform.right * hitForce, hit.point);                    
-                }
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //forceDirection = Random.Range(1, 3);
+                Debug.Log("mouse click");
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //forceDirection = Random.Range(1, 3);
 
-            if (Physics.Raycast(ray, out hit, maxDistance))
-            {
-                if (hit.rigidbody != null)
+                if (Physics.Raycast(ray, out hit, maxDistance))
                 {
-                    hit.rigidbody.velocity = Vector3.zero;
-                    hit.rigidbody.AddForceAtPosition(-transform.right * hitForce, hit.point);
+                    if (hit.rigidbody != null)
+                    {                     
+                        hit.rigidbody.velocity = Vector3.zero;
+                        hit.rigidbody.AddForceAtPosition(transform.right * hitForce, hit.point);                    
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log("mouse click");
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //forceDirection = Random.Range(1, 3);
+
+                if (Physics.Raycast(ray, out hit, maxDistance))
+                {
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.velocity = Vector3.zero;
+                        hit.rigidbody.AddForceAtPosition(-transform.right * hitForce, hit.point);
+                    }
                 }
             }
         }
-
     }
 }
 /*
